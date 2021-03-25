fab7ff1ba0b9e029160b74e1aaafcb3b1cc60bb2
Viktoriia Nosova <viktoriian@wix.com>
2021-03-25 16:47:58 +0200

    Changed launch datetime to be displayed in launch site timezone

--- src/utils/format-date.js
+++ src/utils/format-date.js
@@ -1,20 +1,33 @@
 export function formatDate(timestamp) {
-  return new Intl.DateTimeFormat("en-US", {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  }).format(new Date(timestamp));
+	return new Intl.DateTimeFormat("en-US", {
+		weekday: "long",
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+	}).format(new Date(timestamp));
 }
 
 export function formatDateTime(timestamp) {
-  return new Intl.DateTimeFormat("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-    timeZoneName: "short",
-  }).format(new Date(timestamp));
+	return new Intl.DateTimeFormat("en-US", {
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+		hour: "numeric",
+		minute: "numeric",
+		second: "numeric",
+		timeZoneName: "short",
+	}).format(new Date(timestamp));
+}
+
+export function formatDateTimeWithTimezone(timestamp, timeZone) {
+	return new Intl.DateTimeFormat("en-US", {
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+		hour: "numeric",
+		minute: "numeric",
+		second: "numeric",
+		timeZoneName: "short",
+		timeZone,
+	}).format(new Date(timestamp));
 }
